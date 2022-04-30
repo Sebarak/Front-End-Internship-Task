dc26667be52c33ed2225aa2d4799d5dcb878b784
unknown <sebastian.rak9@gmail.com>
2022-05-01 01:56:14 +0200

    Get access to single user & start validate

--- front-end-internship/src/EditIntern.jsx
+++ front-end-internship/src/EditIntern.jsx
@@ -1,40 +1,80 @@
 import React, {useEffect, useState} from 'react';
-import { useParams } from 'react-router';
+import {useParams, useNavigate} from 'react-router';
 import {NavLink} from 'react-router-dom';
 
 const EditIntern = () => {
-    const { id } = useParams();
-    const [users,setUser] = useState({})
-
+    const {id} = useParams();
+    const [user, setUser] = useState({});
+    const [internshipS, setStart] = useState('');
+    const [internshipE, setEnd] = useState('');
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('');
+    const navigate = useNavigate();
 
     useEffect(() => {
         //TODO: get intern from REST api http://localhost:3001/interns/:id
-        console.log(`I want to get intern with id: ${id}!`);
+
+        fetch(`http://localhost:3001/interns/${id}`)
+            .then(response => response.json())
+            .then(data => setUser(data));
+
     }, [id]);
+
+    const Submit = (event) => {
+        event.preventDefault();
+
+        const requestOptions = {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({name, email, internshipStart: internshipS, internshipEnd: internshipE})
+        };
+
+        if (name !== '' && email !== '' && internshipE !== '' && internshipS !== '' && internshipS < internshipE) {
+            fetch(`http://localhost:3001/interns/${id}`, requestOptions)
+                .then(response => response.json())
+                .then(data => data);
+            setName('');
+            setEmail('')
+            setStart('');
+            setEnd('');
+            navigate('/');
+        }
+
+    }
 
     return (
         <main>
             <div className='container return'>
                 <NavLink to="/" className='return_button'>Back to list </NavLink>
             </div>
-            <form className='container form'>
+            <form className='container form' onSubmit={e => {
+                Submit(e)
+            }}>
                 <section className='form_user_values'>
                     <label className='user_name'>Name *</label>
-                    <input type="text" name="name" className='user_value'/>
+                    <input type="text" name="name" placeholder={user.name} value={name}
+                           onChange={e => setName(e.currentTarget.value)} className='user_value'/>
                     <label className='user_name'>Email *</label>
-                    <input type="text" name="email" className='user_value'/>
+                    <input type="email" name="email" placeholder={user.email} value={email}
+                           onChange={e => setEmail(e.currentTarget.value)} className='user_value'/>
                 </section>
                 <section className='form_internship_date'>
                     <div className='internship_date'>
                         <label>Internship start *</label>
-                        <input type="date" className='user_date start'/>
+                        <input type="text" placeholder={user.internshipStart} value={internshipS}
+                               onChange={e => setStart(e.currentTarget.value)} onFocus={e => (e.target.type = "date")}
+                               onBlur={e => (e.target.type = "text")} min='1990-01-01' max="2030-12-31"
+                               className='user_date start'/>
                     </div>
                     <div className='internship_date'>
                         <label>Internship end *</label>
-                        <input type="date" className='user_date end'/>
+                        <input type="text" placeholder={user.internshipEnd} value={internshipE}
+                               onChange={e => setEnd(e.currentTarget.value)} onFocus={e => (e.target.type = "date")}
+                               onBlur={e => (e.target.type = "text")} min='01-01-2000' max="12-31-2030"
+                               className='user_date end'/>
                     </div>
                 </section>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit"/>
             </form>
         </main>
     );
